9cba477c29063eb01d9398166adfe59663575c57
Sergey Gorbov <sergey.gorbov@aon.com>
2023-05-14 19:48:59 -0500

    convert lamda to function pointer example

--- Chapter02/lambda_types.cpp
+++ Chapter02/lambda_types.cpp
@@ -1,5 +1,20 @@
 #include <gtest/gtest.h>
 
+extern "C" {
+  void download_webpage(const char* url, void (*callback)(int, const char*)); 
+}
+
+TEST(LambdaTypes, ConvertToFunctionPointer) {
+  // auto callback = +[](int status, const char* content) {
+  //                ^^^^ the plus sign isn't required anymore?
+  auto callback = [](int status, const char* content) { // C++20
+    std::cout << "Status: " << status << std::endl;
+    std::cout << "Content: " << content << std::endl;
+  };
+
+  download_webpage("https://www.packtpub.com/", callback);
+}
+
 // Requires C++20
 TEST(LambdaTypes, StatelessLambdas) {
   auto x = [] {}; // A lambda without captures
