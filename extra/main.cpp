92b2776c552c7e113001e3d5e6ae2c9e17782e99
Sergey Gorbov <sergey.gorbov@aon.com>
2023-05-24 15:31:27 -0500

    Poke around with std::views

--- extra/main.cpp
+++ extra/main.cpp
@@ -134,11 +134,16 @@ int my_main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
     | std::views::transform([](int x) { return std::format("\"{}\"", x); })
   );
 
+
   auto csv = std::string{"10,11,12"};
   auto digits = csv
     | std::views::split(',')
     | std::views::join;
   for (auto x : digits) {std::cout << x;}
+
+  auto v3 = std::vector{4, 2, 7, 1, 2, 6, 1, 5};
+  auto first_half = v3 | std::views::take(v3.size() / 2);
+  std::ranges::sort(first_half);
 
   return 0;
 }
